f414138cc0352ebf2a567637cc062dfb74671cf7
topher097 <cmendres400@gmail.com>
2026-03-18 18:01:27 -0400

    add: alpha-nvim for start dashboard

--- nix/neovim-overlay.nix
+++ nix/neovim-overlay.nix
@@ -28,6 +28,7 @@ with final.pkgs.lib; let
   #   ...
   # }
   all-plugins = with pkgs.vimPlugins; [
+    alpha-nvim
     # plugins from nixpkgs go in here.
     # https://search.nixos.org/packages?channel=unstable&from=0&size=50&sort=relevance&type=packages&query=vimPlugins
     nvim-treesitter.withAllGrammars
