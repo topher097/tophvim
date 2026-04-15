217f11ff19c7db69a44e8129d63869ccc19dbbb1
topher097 <cmendres400@gmail.com>
2026-04-14 20:58:35 -0400

    add: keybind for converting visual selections to a checklist, where each line is made into a checklist item

--- nvim/plugin/markdown-plus.lua
+++ nvim/plugin/markdown-plus.lua
@@ -8,3 +8,34 @@ require('markdown-plus').setup {
     enabled = true,
   },
 }
+
+vim.keymap.set('v', '<leader>ms', function()
+  local v_pos = vim.fn.getpos('v')
+  local dot_pos = vim.fn.getpos('.')
+
+  local s = math.min(v_pos[2], dot_pos[2])
+  local e = math.max(v_pos[2], dot_pos[2])
+
+  vim.api.nvim_feedkeys(vim.api.nvim_replace_termcodes('<Esc>', true, false, true), 'nx', false)
+
+  local lines = vim.api.nvim_buf_get_lines(0, s - 1, e, false)
+
+  for i, line in ipairs(lines) do
+    local trimmed = line:match('^%s*(.*)')
+    if trimmed:match('^%- %[ %]') or trimmed:match('^%- %[x%]') then
+      lines[i] = line
+    elseif trimmed:match('^[%*%+%-]') then
+      lines[i] = line:gsub('^%s*([%*%+%-])%s*', '- [ ] ', 1)
+    elseif trimmed:match('^%d+[%).]') then
+      lines[i] = line:gsub('^%s*(%d+[%).])%s*', '- [ ] ', 1)
+    else
+      lines[i] = '- [ ] ' .. line
+    end
+  end
+
+  vim.api.nvim_buf_set_lines(0, s - 1, e, false, lines)
+  vim.api.nvim_feedkeys('gv', 'x', false)
+end, {
+  silent = true,
+  desc = 'Convert selection to checklist',
+})
